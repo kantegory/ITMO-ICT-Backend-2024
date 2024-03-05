d87d1188c2ae993ad80f398e7f45d1ec17fb704a
Tran Duc Minh <67594792+Mesh296@users.noreply.github.com>
2024-03-05 14:58:19 +0300

    Delete homeworks/K33392/Чан Дык Минь/HW2 directory

--- homeworks/K33392/Чан Дык Минь/HW2/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('test-db', 'user', 'pass', {
-    dialect: 'sqlite',
-    host: './dev.sqlite'
-});
-
-module.exports = sequelize;
-
