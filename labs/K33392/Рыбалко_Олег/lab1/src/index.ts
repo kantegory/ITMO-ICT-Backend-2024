87d793deccb88f3e8788cc1b7c1ab170e233ba92
Oleg Rybalko <rybalko.oleg.123@mail.ru>
2024-04-03 14:31:42 +0300

    dotenv and express json middleware

--- labs/K33392/Рыбалко_Олег/lab1/src/index.ts
+++ labs/K33392/Рыбалко_Олег/lab1/src/index.ts
@@ -1,11 +1,15 @@
 import express from 'express'
 import catsRouter from './routes/cats/index.js'
 import sequelize from './providers/db.js'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
+app.use(express.json())
 app.use('/cats', catsRouter)
 
-app.listen(9090, () => {
-  console.log('Listeing on port 9090')
+app.listen(process.env.PORT, () => {
+  sequelize // to not delete after compilation
+  console.log(`Listening on port ${process.env.PORT}`)
 })
 
