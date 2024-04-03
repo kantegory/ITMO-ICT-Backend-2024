b6bdff1a30ace260021410eaaafb9ae2ebc4b2ec
Oleg Rybalko <rybalko.oleg.123@mail.ru>
2024-04-03 14:13:19 +0300

    added sample routes

---
+++ labs/K33392/Рыбалко_Олег/lab1/src/routes/cats/index.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+import { CatsController } from '../../controllers/cats/index.js'
+
+const router = Router()
+const controller = new CatsController()
+
+router.get('/', controller.get)
+router.post('/', controller.post)
+
+export default router
+
