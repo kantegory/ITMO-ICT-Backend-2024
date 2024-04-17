cf28063592e6e0b8f0e646bdbada673a592bfdb5
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2024-04-17 14:45:48 +0300

    feat: Add refresh tokens to authorization methods

--- labs/M3221d/Kutsalo_Aleksandr/lab2/src/middleware/authMiddleware.ts
+++ labs/M3221d/Kutsalo_Aleksandr/lab2/src/middleware/authMiddleware.ts
@@ -1,16 +1,19 @@
 import { config } from 'dotenv'
 import jwt from 'jsonwebtoken'
+import verifyRefreshToken from '../utility/verifyRefreshToken'
+import destroyTokens from '../utility/destroyTokens'
+import makeTokens from '../utility/makeTokens'
 config()
 
-const requireAuth = (req: any, res: any, next: any) => {
+const requireAuth = async (req: any, res: any, next: any) => {
     try {
         const token = req.cookies.jwt
 
         if (token) {
             jwt.verify(token, process.env.SECRET_KEY, async (err, decodedToken) => {
                 if (err) {
-                    res.status(403).send("Invalid token")
-                    console.log(err.message)
+                    // res.status(403).send("Invalid token")
+                    // console.log(err.message)
                 } else {
                     console.log(decodedToken)
                     res.locals.uId = decodedToken.id
@@ -18,10 +21,37 @@ const requireAuth = (req: any, res: any, next: any) => {
                 }
             })
         } else {
-            res.status(403).send("Authorization is required for this page")
+            // res.status(403).send("Authorization is required for this page")
         }
     }
     catch (err) {
+        if (err.name == "TypeError") {
+            // res.status(403).send("Authorization is required for this page")
+        } else {
+            res.status(400).send(err.message)
+        }
+    }
+    // Refresh token time
+    try {
+        const refreshToken = req.cookies.refresh_token
+        if (refreshToken) {
+            // Exists
+            const out = await verifyRefreshToken(refreshToken)
+            const isLegit = out[0]
+            const userId = out[1]
+            if (!isLegit) {
+                destroyTokens(res) // why not
+                res.status(403).send("Authorization is required for this page")
+            } else {
+                // JWT doesn't exist, but Refresh Token exists and is valid. So now we make new ones
+                res.locals.uId = userId
+                makeTokens(res)
+                next()
+            }
+        } else {
+            res.status(403).send("Authorization is required for this page")
+        }
+    } catch (err) {
         if (err.name == "TypeError") {
             res.status(403).send("Authorization is required for this page")
         } else {
