536a200a12db51d49659b180e3c454e12ec03786
Alexey Korotin <sx12fx@gmail.com>
2024-04-22 17:44:38 +0300

    feat: add auth validation + documentation

--- labs/K33392/Korotin_Alexey/lab2/auth/src/index.ts
+++ labs/K33392/Korotin_Alexey/lab2/auth/src/index.ts
@@ -2,6 +2,9 @@ import express from "express";
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import routes from "./presentation/http";
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
 
 const DEFAULT_HOST = "localhost";
 const DEFAULT_PORT = 8000;
@@ -14,5 +17,30 @@ app.use('/v1', routes);
 
 const appHost = process.env["app.host"] ?? DEFAULT_HOST;
 const appPort = Number(process.env["app.port"]) ?? DEFAULT_PORT;
+
+const swaggerOptions: swaggerJsDoc.Options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Account API',
+            description: "User account API for Smart Device App",
+            version: '1.0.0-BETA',
+            contact: {
+                name: "Alexey Korotin K33392"
+            },
+        },
+        servers: [
+            {
+                url: `http://${appHost}:${appPort}/v1`
+            }
+        ]
+
+    },
+    apis: ['**/presentation/http/*/*.ts']
+};
+
+const docs = swaggerJsDoc(swaggerOptions);
+
+app.use('/swagger-ui', swaggerUi.serve, swaggerUi.setup(docs));
 
 app.listen(appPort, appHost, () => console.log(`Application have started listening ${appHost} at port ${appPort}`));
--- labs/K33392/Korotin_Alexey/lab2/auth/src/presentation/http/auth/Controller.ts
+++ labs/K33392/Korotin_Alexey/lab2/auth/src/presentation/http/auth/Controller.ts
@@ -4,35 +4,103 @@ import {AccountCreateAttributes} from "../../../domain/account/Attributes";
 import {Request, Response} from "express";
 import bcrypt from 'bcrypt';
 import {encodeSession, PartialSession} from "../../../application/services/JWT";
-
+import {InvalidEmailException} from "../../../domain/account/EmailAddress";
+import {validationResult} from 'express-validator';
 export default class AuthController {
 
     public constructor(private repository: AccountRepository,
                        private factory: AccountFactory = new AccountFactory()) {
     }
 
+    /**
+     * @swagger
+     * /auth/register:
+     *      post:
+     *          tags: [Auth]
+     *          summary: Create account and receive a JWT
+     *          requestBody:
+     *              description: Authentication credentials
+     *              required: true
+     *              content:
+     *                  application/json:
+     *                      schema:
+     *                          $ref: '#components/schemas/Credentials'
+     *          responses:
+     *              201:
+     *                  description: Account created successfully
+     *                  content:
+     *                      application/json:
+     *                          schema:
+     *                              $ref: '#components/schemas/JWT'
+     *              400:
+     *                  description: Invalid credentials provided
+     *                  content:
+     *                      application/json:
+     *                          schema:
+     *                              type: object
+     *                              properties:
+     *                                  message:
+     *                                      type: string
+     *              409:
+     *                  description: Conflict. Email is already taken
+     */
     register = async (req: Request, res: Response) => {
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            return res.status(400).json({errors: result.array()});
+        }
         const attributes: AccountCreateAttributes = req.body;
         attributes.password = bcrypt.hashSync(attributes.password, 10);
-        let account = this.factory.create(attributes);
         try {
+            let account = this.factory.create(attributes);
             account = await this.repository.save(account);
-        } catch (e: any) {
+            const session: PartialSession = {
+                sub: account.id,
+                email: account.email.value,
+                role: account.role,
+                status: account.status
+            };
+
+            const result = encodeSession(session);
+            return res.status(201).json(result);
+
+        }  catch (e: any) {
+            if (e instanceof InvalidEmailException) {
+                return res.status(400).json({message: "Email is invalid"});
+            }
             return res.status(409).send();
         }
-
-        const session: PartialSession = {
-            sub: account.id,
-            email: account.email.value,
-            role: account.role,
-            status: account.status
-        };
-
-        const result = encodeSession(session);
-        res.status(201).json(result);
     }
-
-    public login = async (req: Request, res: Response) => {
+    /**
+     * @swagger
+     * /auth/login:
+     *      post:
+     *          tags: [Auth]
+     *          summary: Log in into the existing account
+     *          requestBody:
+     *              description: Authentication credentials
+     *              required: true
+     *              content:
+     *                  application/json:
+     *                      schema:
+     *                          $ref: '#components/schemas/Credentials'
+     *          responses:
+     *              200:
+     *                  description: Ok
+     *                  content:
+     *                      application/json:
+     *                          schema:
+     *                              $ref: '#components/schemas/JWT'
+     *              400:
+     *                  description: Bad request
+     *              401:
+     *                  description: Invalid credentials
+     */
+    login = async (req: Request, res: Response) => {
+        const bindingResult = validationResult(req);
+        if (!bindingResult.isEmpty()) {
+            res.status(400).json({errors: bindingResult.array()});
+        }
         const attributes: AccountCreateAttributes = req.body;
 
         const existing = await this.repository.findByEmail(attributes.email)
