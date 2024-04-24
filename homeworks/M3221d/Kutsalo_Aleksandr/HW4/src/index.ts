b28e113ca20d91b1f76d884ff143d4e2ebd7e8f3
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2024-04-24 14:48:04 +0300

    feat: Create Swagger documentation for user and shop routers

--- homeworks/M3221d/Kutsalo_Aleksandr/HW4/src/index.ts
+++ homeworks/M3221d/Kutsalo_Aleksandr/HW4/src/index.ts
@@ -4,6 +4,8 @@ import mainRouter from "./routers/router"
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import path from 'path'
+// import * as swaggerConfig from './swagger.json'
+import swaggerUi from 'swagger-ui-express'
 
 const app = express()
 
@@ -17,6 +19,9 @@ app.set('views', path.join(path.resolve(), 'src/views'));
 app.use(bodyParser.json())
 app.use(cookieParser())
 
+
+app.use('/api', swaggerUi.serve)
+// app.use('/api', swaggerUi.setup(swaggerConfig))
 app.use('/', mainRouter)
 
 app.listen(port, () => {
