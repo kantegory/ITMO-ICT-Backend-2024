ce6357facf7917a513d5a053e247824c65933462
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2024-05-15 17:16:28 +0300

    feat: Change middleware for communicating with other microservice

--- labs/M3221d/Kutsalo_Aleksandr/lab3/store/src/middleware/authMiddleware.ts
+++ labs/M3221d/Kutsalo_Aleksandr/lab3/store/src/middleware/authMiddleware.ts
@@ -1,64 +1,27 @@
 import jwt from 'jsonwebtoken'
-import verifyRefreshToken from '../utility/verifyRefreshToken'
 import destroyTokens from '../utility/destroyTokens'
-import makeTokens from '../utility/makeTokens'
+import axios from "axios";
 
 const requireAuth = async (req: any, res: any, next: any) => {
     try {
         const token = req.cookies.jwt
-
-        if (token) {
-            jwt.verify(token, process.env.SECRET_KEY, async (err, decodedToken) => {
-                if (err) {
-                    // res.status(403).send("Invalid token")
-                    // console.log(err.message)
-                } else {
-                    console.log(decodedToken)
-                    res.locals.uId = decodedToken.id
-                    next()
-                }
-            })
-        } else {
-            // res.status(403).send("Authorization is required for this page")
-        }
-    }
-    catch (err) {
-        if (err.name == "TypeError") {
-            // res.status(403).send("Authorization is required for this page")
-        } else {
-            res.status(400).send(err.message)
-            return
-        }
-    }
-    // Refresh token time
-    try {
         const refreshToken = req.cookies.refresh_token
-        if (refreshToken) {
-            // Exists
-            const out = await verifyRefreshToken(refreshToken)
-            const isLegit = out[0]
-            const userId = out[1]
-            if (!isLegit) {
-                destroyTokens(res) // why not
-                res.status(403).send("Authorization is required for this page")
-                return
-            } else {
-                // JWT doesn't exist, but Refresh Token exists and is valid. So now we make new ones
-                res.locals.uId = userId
-                makeTokens(res)
-                next()
+
+        const authResponse = await axios.post(
+            "http://localhost:4002/users/auth",
+            {
+                "jwt": token,
+                "refresh_token": refreshToken
             }
-        } else {
-            res.status(403).send("Authorization is required for this page")
+        )
+        if (authResponse.status != 200) {
+            res.status(403).send({"response": "Unauthorized"})
             return
         }
-    } catch (err) {
-        if (err.name == "TypeError") {
-            res.status(403).send("Authorization is required for this page")
-        } else {
-            res.status(400).send(err.message)
-        }
-        return
+        res.locals.uId=authResponse.data.uid
+        next()
+    } catch (error) {
+      res.status(401).json({"response": error.message})
     }
  
  }
