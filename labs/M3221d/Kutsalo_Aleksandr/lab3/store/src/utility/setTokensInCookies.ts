80ee5e69a2913acfbc57a7b8d7fd4d9b5adfa2f7
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2024-05-15 17:50:46 +0300

    quickfix: Please tslint that is mad for some reason

--- labs/M3221d/Kutsalo_Aleksandr/lab3/store/src/utility/setTokensInCookies.ts
+++ labs/M3221d/Kutsalo_Aleksandr/lab3/store/src/utility/setTokensInCookies.ts
@@ -1,7 +1,7 @@
 import RefreshToken from "../models/RefreshToken";
 
 
-function setTokensInCookies(res: any, jwt: string, refreshToken?: RefreshToken): void {
+export default function setTokensInCookies(res: any, jwt: string, refreshToken?: RefreshToken): void {
     res.cookie('jwt', jwt, {httpOnly: true, maxAge: Number(process.env.TOKEN_AGE_MS)})
     if (refreshToken) {
         res.cookie('refresh_token', refreshToken.token, {
@@ -10,5 +10,3 @@ function setTokensInCookies(res: any, jwt: string, refreshToken?: RefreshToken):
         })
     }
 }
-
-export default setTokensInCookies
