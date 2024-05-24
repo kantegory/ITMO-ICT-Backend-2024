3820d57e15a28b8e29a5ca58f90b8c3d436f441f
Nikita <tyumin000@gmail.com>
2024-05-24 13:50:39 +0300

    refactors

---
+++ labs/K33402/Тюмин Никита/lab4/shared/core/dist/responses/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ApiResponse = void 0;
+const apiResponse_1 = __importDefault(require("./apiResponse"));
+exports.ApiResponse = apiResponse_1.default;
