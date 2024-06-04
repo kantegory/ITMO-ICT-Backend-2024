cfb8bc5d9a8950d4a4f1409edf498ee6653d72b9
HelBroone <87618005+HelBroone@users.noreply.github.com>
2024-06-04 20:27:54 +0300

    update

--- homeworks/K33392/Скороходова Елена/hw6/otherfunctions/dist/src/config/database.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sequelize = void 0;
-const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize({
-    dialect: 'sqlite',
-    storage: 'db.sqlite',
-});
-exports.sequelize = sequelize;
