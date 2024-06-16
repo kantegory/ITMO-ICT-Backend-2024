7cf71c2390ef179482d1e4196be8246c34b05bcb
Aleksandr Kutsalo <60144890+Ssanyoq@users.noreply.github.com>
2024-06-16 12:06:58 +0300

    Принимает отчёт: Куцало Александр M3221d Lab2 (#76)
    * Initial commit
    
    * feat: Add database models and their initializations
    
    * feat: Add pug preprocessor and shop router
    
    * feat: Add test router and controller for shop
    
    * refactor: Rename lab3 to lab2
    
    * issue: Adminjs problem
    
    * issue: Adminjs problem
    
    * temporary: Remove adminjs parts from router
    
    * feat: Use repository instead of direct call of DB model
    
    * feat: Add self-written admin router, rename not working one
    
    * feat: Move decode_token.ts contents to auth_middleware, add user ID to response locals
    
    * feat: Add response locals showcase
    
    * feat: Add shopping cart routes, controllers and services methods
    
    * feat: Add Tag, associative model for Item and tweak Item for Tag usage
    
    * feat: Add Tag and ItemTag to sequelize initialization
    
    * feat: add routes for tag management and association, add get all items route
    
    * feat: add filtration by tag
    
    * feat: Use .env instead of .ts file for configuration
    
    * bugfix: Fix items getter so that it works with tags correctly
    
    * style: Remove unused imports, typescriptify code and do other tweaks suggested by eslint
    
    * fix: add dotenv configs to everything
    
    * refactor: Rename snake_case to camelCase or PascalCase for models
    
    * feat: Add Refresh Token model and uuidv4 for its functionality
    
    * feat: Change token value on instance update
    
    * feat: Add refresh tokens to authorization methods
    
    * style: Prettify dotenv configuration and remove boilerplate code
    
    * feat: Integrate refresh token in logging in and creation of user
    
    * feat: Add cart stacking and fix auth
    
    * feat: Add report

---
+++ labs/M3221d/Kutsalo_Aleksandr/lab2/src/controllers/shop.ts
@@ -0,0 +1,112 @@
+import { Request, Response } from "express";
+import Item from "../models/Item";
+import ShopService from "../services/shop";
+import UserService from "../services/user";
+
+
+class ShopController {
+    
+    
+    private shopService: ShopService
+    private userService: UserService
+
+    constructor() {
+        this.shopService = new ShopService()
+        this.userService = new UserService()
+    }
+
+    index = (request: Request, response: Response) => {
+        response.render('shop/index', {title: 'World!'})
+    }
+
+    addItem = async (request: Request, response: Response) => {
+        try {
+            const item: Item | Error = await this.shopService.addItem(request.body)
+
+            response.status(200).json({'response': "success", 'itemId': item.id})
+            return
+        } catch (error) {
+            response.status(400).json({'response': 'error', 'error_message': error.message})
+            return
+        }
+    }
+    getItem = async (request: Request, response: Response) => {
+        try {
+            const user: Item | Error = await this.shopService.getById(Number(request.params.id))
+            response.status(200).json(user)
+        } catch (error) {
+            response.status(404).json({"response": "error", "error_message": error.message})
+            return
+        }
+    }
+    deleteItem = async (request: Request, response: Response) => {
+        try {
+            await this.shopService.deleteById(Number(request.params.id))
+            response.status(200).json({'response': "success"})
+        } catch (error) {
+            console.log(error)
+            response.status(404).json({"response": "error", "error_message": error.message})
+            return
+        }
+    }
+    getAllItems = async (request: Request, response: Response) => {
+        try {
+            const items = await this.shopService.getItems(request.body)
+
+            response.status(200).json(items)
+        } catch (error) {
+            response.status(404).json({"response": "error", "error_message": error.message})
+            return
+        }
+    }
+
+    addToShoppingCart = async (request: Request, response: Response) => {
+        try {
+            const cartItem = await this.shopService.addToCart(Number(request.params.id), response.locals.uId, request.body.quantity)
+            response.status(200).json({'response': "success", cartItem})
+        } catch (error) {
+            console.log(error)
+            response.status(400).json({"response": "error", "error_message": error.message})
+        }
+    }
+
+    getShoppingCartContents = async (request: Request, response: Response) => {
+        try {
+            const items = await this.shopService.getShoppingCartContents(response.locals.uId)
+            response.status(200).json(items)
+        } catch (error) {
+            console.log(error)
+            response.status(400).json({"response": "error", "error_message": error.message})
+        }
+    }
+
+    makeAssociation = async (request: Request, response: Response) => {
+        try {
+            const association = await this.shopService.makeAssociation(Number(request.body.itemId), Number(request.body.tagId))
+            response.status(200).json(association)
+        } catch (error) {
+            console.log(error)
+            response.status(400).json({"response": "error", "error_message": error.message})
+        }
+    }
+    getTags = async (request: Request, response: Response) => {
+        try {
+            const tags = await this.shopService.getTags()
+            response.status(200).json(tags)
+        } catch (error) {
+            console.log(error)
+            response.status(400).json({"response": "error", "error_message": error.message})
+        }
+    }
+    addTag = async (request: Request, response: Response) => {
+        try {
+            const tag = await this.shopService.addTag(request.body)
+            response.status(200).json(tag)
+        } catch (error) {
+            console.log(error)
+            response.status(400).json({"response": "error", "error_message": error.message})
+        }
+    }
+}
+
+export default ShopController
